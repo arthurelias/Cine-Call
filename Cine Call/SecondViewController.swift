5177820fa8d110a3e7278e75c19c81e258a99c58
MacBook <macbook@macbooks-macbook.local>
2016-09-13 14:37:50 -0300

    Initial Commit

---
+++ Cine Call/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cine Call
+//
+//  Created by MacBook on 9/13/16.
+//  Copyright © 2016 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
